3dac8a57b53c94a29862710342a749dc4bb1b49a
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-04-02 10:24:18 -0400

    add empty methods to piece class

--- piece.rb
+++ piece.rb
@@ -1,11 +1,24 @@
 class Piece
 
-  attr_accessor :king, :board
-  def initialize(board, king = false)
-    @king = king
+  attr_accessor :king, :board, :color
+  def initialize(board, color, king = false)
     @board = board
+    @color = color
+    @king = king
   end
 
-  
+  def perform_slide
+
+  end
+
+  def perform_jump
+
+  end
+
+  def move_diffs
+
+  end
+
+
 
 end
