a84494813578861dfd5e856bf87331894c2926b7
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-04-02 16:11:57 -0400

    add peform_moves! method to piece

--- piece.rb
+++ piece.rb
@@ -44,7 +44,12 @@ class Piece
     self.position = to_pos
   end
 
-  
+  # def move!(to_pos)
+  #   new_board = board.dup
+  #   new_board[self.position] = nil
+  #   new_board[to_pos] = self.dup
+  #   new_board[to_pos].position = to_pos
+  # end
 
   def available_slide_moves
     move_diffs.map do |diff|
@@ -59,7 +64,7 @@ class Piece
       next_space = adjacent_space.zip_sum(diff)
       square = board[adjacent_space]
       if square.is_a?(Piece) && square.color != color && board[next_space].nil?
-        moves << next_space #should be next_space
+        moves << next_space
       end
     end
 
@@ -67,8 +72,17 @@ class Piece
   end
 
   def perform_moves!(move_sequence)
-    move_sequence.each do |move|
-      break if perform_slide(moves)
+    new_board = board.dup
+    if move_sequence.size == 1
+      break if new_board[self.position].perform_slide(move_sequence.first)
+      new_board[self.position].perform_jump(move_sequence.first)
+    elsif move_sequence.size > 1
+      move_sequence.each do |move|
+        new_board[self.position].perform_jump(move)
+      end
+    end
+  rescue
+    raise InvalidMoveError
   end
 
   def move_diffs
