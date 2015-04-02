e689257aba58a1b9aac9c3122740f33ebd88833a
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-04-02 10:38:41 -0400

    add render method to board

--- board.rb
+++ board.rb
@@ -1,13 +1,38 @@
+require 'colorize'
 class Board
 
   attr_accessor :rows
-  
+
   def initialize
     make_starting_grid
   end
 
   def make_starting_grid
     @rows = Array.new(8) {Array.new(8)}
+  end
+
+  def render
+    is_white = true
+
+    display_str = ""
+    @rows.each do |row|
+      row.each do |piece|
+        if piece
+          display_str << piece.render
+        elsif is_white
+          display_str << "  ".colorize(:background => :white)
+        else
+          display_str << "  ".colorize(:background => :light_black)
+        end
+      end.join("")
+    end.join("\n")
+
+    display_str
+  end
+
+  def add_starting_pieces
+    @rows.each do |row|
+      row
   end
 
 end
